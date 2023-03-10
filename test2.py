afed9fa6959d66625f4671bfb28bd91603917ff8
lilkhach <126867623+lilkhach@users.noreply.github.com>
2023-03-10 12:17:28 +0400

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,19 @@
+print("Fall")
+x = 3
+y = 4
+t = (x*x)+(y*y)
+z = t**(1/2)
+
+print(z == 5)
+
+x = 4.5
+y = 7.8
+z = (x*x + y*y) **(1/2)
+print (z)
+
+x = 7
+y = 8
+z = (x**2+ y**2) **(1/2)
+print (z)
+
+
