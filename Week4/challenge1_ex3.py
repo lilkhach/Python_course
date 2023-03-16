92df9f5e7426faa1fbb059998d46a2a884133840
lilkhach <126867623+lilkhach@users.noreply.github.com>
2023-03-16 23:54:19 +0400

    Add files via upload

---
+++ Week4/challenge1_ex3.py
@@ -0,0 +1,16 @@
+"""3. Write a function that takes an integer minutes and converts it to seconds.
+5 ➞ 300
+2 ➞ 120
+"""
+
+x = int(5) 
+
+x = x * 60
+
+print(x)
+
+x = int(2) 
+
+x = x * 60
+
+print(x)
