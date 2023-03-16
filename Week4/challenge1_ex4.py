3e501bdb6f761d57486d22f093bc279ff72dff53
lilkhach <126867623+lilkhach@users.noreply.github.com>
2023-03-16 23:55:27 +0400

    Add files via upload

---
+++ Week4/challenge1_ex4.py
@@ -0,0 +1,26 @@
+"""4. Create a function that takes the age in years and returns the age in days.
+65 ➞ 23725
+0 ➞ 0
+20 ➞ 7300
+"""
+
+
+x = int(65) 
+
+x = x * 365
+
+print(x)
+
+
+x = int(0) 
+
+x = x * 365
+
+print(x)
+
+
+x = int(20) 
+
+x = x * 365
+
+print(x)
