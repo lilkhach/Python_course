40ec51d4ab04a9cf7fb441a059314249de59694c
lilkhach <126867623+lilkhach@users.noreply.github.com>
2023-03-16 23:56:29 +0400

    Add files via upload

---
+++ Week4/challenge1_ex6.py
@@ -0,0 +1,9 @@
+"""6. Create a function that accepts a measurement value in inches and returns the equivalent of the measurement value in feet."""
+
+inches = int(input("value of length in inches"))
+
+feet = inches / 12
+
+print(feet)
+
+
