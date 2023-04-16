d8225c974afd412b6021765eac0701b3ba8b3ce3
lilkhach <lilit.khachatryan.botany@gmail.com>
2023-04-17 03:14:45 +0400

    Adding solutions to week7 challenge2

--- Week7/challenge2.py
+++ Week7/challenge2.py
@@ -11,20 +11,20 @@ Return 0 if given an empty array.
 All array items are of the type bool (true or false). """
 
 
-# def countTrue(array: list):
-#     count = 0
-#     for i in array:
-#         if i:
-#             count += 1
-#     return count
+def countTrue(array: list):
+    count = 0
+    for i in array:
+        if i:
+            count += 1
+    return count
 
 
 
-# print(countTrue([True, False, False, True, False]) == 2)
+print(countTrue([True, False, False, True, False]) == 2)
 
-# print(countTrue([False, False, False, False]) == 0)
+print(countTrue([False, False, False, False]) == 0)
 
-# print(countTrue([]) == 0)
+print(countTrue([]) == 0)
 
 
 
@@ -42,19 +42,19 @@ Bounds will be always given as integers. """
 
 
 
-# def intWithinBounds(n, lower, upper):
+def intWithinBounds(n, lower, upper):
 
-#     if not isinstance(n, int):
-#         return False
-#     return lower <= n < upper
+    if not isinstance(n, int):
+        return False
+    return lower <= n < upper
 
 
 
-# print(intWithinBounds(3, 1, 9) == True)
+print(intWithinBounds(3, 1, 9) == True)
 
-# print(intWithinBounds(6, 1, 6) == False)
+print(intWithinBounds(6, 1, 6) == False)
 
-# print(intWithinBounds(4.5, 3, 8) == False)
+print(intWithinBounds(4.5, 3, 8) == False)
 
 
 
@@ -73,17 +73,17 @@ longestTime(2, 300, 15000) ➞ 300
 longestTime(15, 955, 59400) ➞ 59400 """
 
 
-# def longestTime(h, m, s):
+def longestTime(h, m, s):
 
-#     long_dur = max(h * 3600, m * 60, s)
-#     return long_dur
+    long_dur = max(h * 3600, m * 60, s)
+    return long_dur
 
 
-# print(longestTime(1, 59, 3598)) #== 1)
+print(longestTime(1, 59, 3598)) #== 1)
 
-# print(longestTime(2, 300, 15000)) #== 300)
+print(longestTime(2, 300, 15000)) #== 300)
 
-# print(longestTime(15, 955, 59400) == 59400)
+print(longestTime(15, 955, 59400) == 59400)
 
 
 
@@ -102,26 +102,26 @@ Don't forget about leap years! """
 
 
 
-# def day_count (month: int, year: int):
-#     if month == 2:
-#         if (year % 4 == 0 and year % 100 != 0):
-#             return 29  # leap year
-#         else:
-#             return 28
-#     elif month in [4, 6, 9, 11]:
-#         return 30
-#     else:
-#         return 31
+def day_count (month: int, year: int):
+    if month == 2:
+        if (year % 4 == 0 and year % 100 != 0):
+            return 29  # leap year
+        else:
+            return 28
+    elif month in [4, 6, 9, 11]:
+        return 30
+    else:
+        return 31
 
 
 
-# print(day_count(2, 2024) == 29)
+print(day_count(2, 2024) == 29)
 
-# print(day_count(4, 654) == 30)
+print(day_count(4, 654) == 30)
 
-# print(day_count(2, 200) ==28)
+print(day_count(2, 200) ==28)
 
-# print(day_count(2, 1000) == 28)
+print(day_count(2, 1000) == 28)
 
 
 
@@ -140,24 +140,24 @@ The amount of * is the same as the length of the word. """
 
 
 
-# def censor(exp: str):
-#     lst_exp = exp.split()
-#     new_exp = []
-#     for i in lst_exp:
-#         if len(i) > 4:
-#             new_word = "*" * len(i)
-#             new_exp.append(new_word)
-#         else:
-#             new_exp.append(i)
-#     return " ".join(new_exp)
+def censor(exp: str):
+    lst_exp = exp.split()
+    new_exp = []
+    for i in lst_exp:
+        if len(i) > 4:
+            new_word = "*" * len(i)
+            new_exp.append(new_word)
+        else:
+            new_exp.append(i)
+    return " ".join(new_exp)
 
 
 
-# print(censor("The code is fourty") == "The code is ******")
+print(censor("The code is fourty") == "The code is ******")
 
-# print(censor("Two plus three is five") == "Two plus ***** is five")
+print(censor("Two plus three is five") == "Two plus ***** is five")
 
-# print(censor("aaaa aaaaa 1234 12345") == "aaaa ***** 1234 *****")
+print(censor("aaaa aaaaa 1234 12345") == "aaaa ***** 1234 *****")
 
 
 
@@ -175,26 +175,26 @@ Notes
 Watch for uppercase letters as shown in example #3. """
 
 
-# def absolute(sent: str):
+def absolute(sent: str):
 
-#     lst_sent = sent.split()
+    lst_sent = sent.split()
 
-#     for i in range(len(lst_sent)):  
+    for i in range(len(lst_sent)):  
 
-#         if lst_sent[i] == "a":
-#             lst_sent[i] = "an absolute"  
+        if lst_sent[i] == "a":
+            lst_sent[i] = "an absolute"  
 
-#         elif lst_sent[i] == "A":
-#             lst_sent[i] = "An absolute"  
+        elif lst_sent[i] == "A":
+            lst_sent[i] = "An absolute"  
 
-#     return " ".join(lst_sent)
+    return " ".join(lst_sent)
         
 
-# print(absolute("I am a champion!!!") == "I am an absolute champion!!!")
+print(absolute("I am a champion!!!") == "I am an absolute champion!!!")
 
-# print(absolute("Such an amazing bowler.") == "Such an amazing bowler.")
+print(absolute("Such an amazing bowler.") == "Such an amazing bowler.")
 
-# print(absolute("A man with no haters.") == "An absolute man with no haters.")
+print(absolute("A man with no haters.") == "An absolute man with no haters.")
 
 
 
@@ -217,22 +217,22 @@ The first two arguments will always be integers.
 The third argument is either a string or an integer. """
 
 
-# def matrix(x, y, z):
+def matrix(x, y, z):
 
-#     output = []
+    output = []
 
-#     for i in range(x):
-#         subarray = [z] * y
-#         output.append(subarray)
+    for i in range(x):
+        subarray = [z] * y
+        output.append(subarray)
 
-#     return output
+    return output
 
 
-# print(matrix(3, 2, 3) == [[3, 3], [3, 3], [3, 3]])
+print(matrix(3, 2, 3) == [[3, 3], [3, 3], [3, 3]])
 
-# print(matrix(2, 1, "edabit") == [["edabit"], ["edabit"]])
+print(matrix(2, 1, "edabit") == [["edabit"], ["edabit"]])
 
-# print(matrix(3, 2, 0) == [[0, 0], [0, 0], [0, 0]])
+print(matrix(3, 2, 0) == [[0, 0], [0, 0], [0, 0]])
 
 
 
@@ -250,19 +250,19 @@ Notes
 Bonus: Try to complete this challenge in one line! """
 
 
-# def multiplyNums(string):
+def multiplyNums(string):
 
-#     return eval("*".join(string.split(", ")))
+    return eval("*".join(string.split(", ")))
 
 
 
-# print(multiplyNums("2, 3") == 6)
+print(multiplyNums("2, 3") == 6)
 
-# print(multiplyNums("1, 2, 3, 4") == 24)
+print(multiplyNums("1, 2, 3, 4") == 24)
 
-# print(multiplyNums("54, 75, 453, 0") == 0)
+print(multiplyNums("54, 75, 453, 0") == 0)
 
-# print(multiplyNums("10, -2") == -20)
+print(multiplyNums("10, -2") == -20)
 
 
 
@@ -279,19 +279,19 @@ Notes
 Return "No car available" if there is no car on the road and "No road available" if there is no road."""
 
 
-# def firstPlace(road_string):
-#     if not road_string:
-#         return "No road available"  
-#     elif all(i == '=' for i in road_string):
-#         return "No car available"
+def firstPlace(road_string):
+    if not road_string:
+        return "No road available"  
+    elif all(i == '=' for i in road_string):
+        return "No car available"
     
-#     return road_string.replace("=", "")[-1]
+    return road_string.replace("=", "")[-1]
 
 
 
-# print(firstPlace("====b===O===e===U=A=="))
-# print(firstPlace("e==B=Fe"))
-# print(firstPlace(""))
+print(firstPlace("====b===O===e===U=A=="))
+print(firstPlace("e==B=Fe"))
+print(firstPlace(""))
 
 
 """10. Create a function that takes an array of numbers between 1 and 10 (excluding one number) and returns the missing number.
@@ -311,7 +311,9 @@ Only one number will be missing."""
 def missingNum(array):
 
     full_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
     missing_number = set(full_arr) - set(array)
     return int(missing_number.pop())
+
 
 print(missingNum([7, 2, 3, 6, 5, 9, 1, 4, 8]))
