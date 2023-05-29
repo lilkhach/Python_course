1646892446d1c348f0d1af38868baaa03ffd076a
lilkhach <lilit.khachatryan.botany@gmail.com>
2023-05-30 03:02:41 +0400

    adding new file

---
+++ reduce_map_filter.py
@@ -0,0 +1,9 @@
+"""1.	Write a map function that adds plus 5 to each item in the list.
+2.	Write a map function that adds "Hello, " in front of each item in the list.
+3.	Using filter() function filter the list so that only negative numbers are left.
+4.	Using filter function, filter the even numbers so that only odd numbers are passed to the new list
+5.	Using map() and filter() functions add 2000 to the values below 8000.
+6.	Return product of integer lists"""
+
+
+
